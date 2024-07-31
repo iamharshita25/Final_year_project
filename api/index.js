8fa8d658ec5281a16249d25a0c5d58b003e95614
Palak Agarwal <112793075+palakag29@users.noreply.github.com>
2024-07-31 16:15:59 +0000

    init

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,7 +16,7 @@ mongoose.connect(mongoURI)
   });
 
 const app = express();
-
+app.use('/api/user', userRouter);
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
   });
